18eb101b498011a01e8327bb2983989ad932a82d
Nassim <nassim19961996@outlook.fr>
2019-02-28 11:22:54 +0100

    First commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Movie;
+use App\Entity\Evaluation;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class TestController extends AbstractController
+{
+
+
+    /**
+     * fonction fète pr tester ds trucs
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        $ms = $this->getDoctrine()->getRepository(Movie::class)->findAll();
+        //fonction qui essé de calc moyen note flm mais prblm
+        for ($i=0; $i < count($ms) ; $i) {
+          $notes = $ms[$i]->getEvaluations()->getGrade();
+        }
+        return $this->render('test/index.html.twig', [
+          "ms" => $ms
+        ]);
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Movie::class);
+        $movies = $repository->findAll();
+        return $this->render('test/index.html.twig', [
+          "movies" => $movies
+        ]);
+    }
+
+    /**
+     * @Route("/single/{id}", name="single")
+     */
+    public function show(Movie $movie)
+    {
+        return $this->render('test/single.html.twig', [
+          "movie" => $movie
+        ]);
+    }
+
+    /**
+     * @Route("/evaluation/{id}", name="evaluation")
+     * @IsGranted("ROLE_USER")
+     */
+    public function rate(Movie $movie, Request $request)
+    {
+        $evaluation = new Evaluation();
+
+        $form = $this->createFormBuilder($evaluation)
+            ->add('comment')
+            ->add('grade')
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $evaluation->setMovie($movie);
+          $evaluation->setUser($this->getUser());
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->persist($evaluation);
+          $entityManager->flush();
+        }
+
+        return $this->render('test/evaluation.html.twig', [
+          "movie" => $movie,
+          "form" => $form->createView()
+        ]);
+    }
+}
---
+++ src/DataFixtures/MovieFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+// use Doctrine\Bundle\Faker;
+//Ne c ps kel é le bon faker
+use App\Entity\Movie;
+use Faker;
+
+class MovieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+      $faker = Faker\Factory::create('fr_FR');
+
+      for ($i=0; $i < 20; $i++) {
+        $movie = new Movie();
+        $movie->setTitle($faker->realText($maxNbChars = 50, $indexSize = 1));
+        $movie->setSumary($faker->text($maxNbChars = 400));
+        $movie->setReleaseYear(new \DateTime($faker->date($format = 'Y-m-d', $max = 'now')));
+        $movie->setType("Horror");
+        $movie->setAuthor($faker->firstNameMale() . " " . $faker->lastName());
+        $manager->persist($movie);
+      }
+      $manager->flush();
+    }
+}
